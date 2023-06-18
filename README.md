fecc2853e15b08e067a96cb83d9fdf6f804111e6
vinesh-yokentiran <72181010+vinesh-yokentiran@users.noreply.github.com>
2023-06-19 10:29:58 +1200

    Added project files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+### Music Player App
+A simple music player web application coded in HTML, CSS, and JavaScript. 
+
+# Preview
+Here is the preview of the project.
+
+![screenshot](screenshot.jpg)
---
+++ index.js
@@ -0,0 +1,102 @@
+const image = document.getElementById('cover'),
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next'),
+    playBtn = document.getElementById('play'),
+    background = document.getElementById('bg-img');
+
+const music = new Audio();
+
+const songs = [
+    {
+        path: 'assets/Am I Dreaming.mp3',
+        displayName: 'Am I Dreaming',
+        cover: 'assets/Am I Dreaming.jpeg',
+        artist: 'Metro Boomin, A$AP Rocky, Roisee',
+    },
+    {
+        path: 'assets/Hooked on a feeling.mp3',
+        displayName: 'Hooked on a feeling',
+        cover: 'assets/Hooked on a feeling.jpeg',
+        artist: 'Blue Swede, Bjornn Skifs',
+    },
+    {
+        path: 'assets/Flowers.mp3',
+        displayName: 'Flowers',
+        cover: 'assets/Flowers.jpeg',
+        artist: 'Khai Dreams',
+    }
+];
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay() {
+    if (isPlaying) {
+        pauseMusic();
+    } else {
+        playMusic();
+    }
+}
+
+function playMusic() {
+    isPlaying = true;
+    // Change play button icon
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseMusic() {
+    isPlaying = false;
+    // Change pause button icon
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+function loadMusic(song) {
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
+}
+
+function changeMusic(direction) {
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic();
+}
+
+function updateProgressBar() {
+    const { duration, currentTime } = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
